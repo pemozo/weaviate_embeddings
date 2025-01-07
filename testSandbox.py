5bad516dfc0283b719bda247ff2f4ff45ea798b3
pemozo <zoghian.mohammed@fh-swf.de>
2025-01-08 00:50:41 +0100

    init repo

---
+++ testSandbox.py
@@ -0,0 +1,22 @@
+# import weaviate
+# import os
+
+print('Test Sandbox')
+
+# client = weaviate.Client(
+#     url = "https://akvhntvs8klnd5sapbxaw.c0.europe-west3.gcp.weaviate.cloud",  # Replace with your endpoint
+#     auth_client_secret=weaviate.AuthApiKey(api_key="dkN1j8XUMvt3F7YQOqRxp4yrqOTe1SRajdW"),  # Replace w/ your Weaviate instance API key
+# )
+
+# # Verbindung überprüfen
+# try:
+#     if client.is_ready():
+#         print("Erfolgreich mit Weaviate Cloud verbunden!")
+#     else:
+#         print("Die Verbindung zu Weaviate ist nicht bereit.")
+# except Exception as e:
+#     print(f"Fehler bei der Verbindung: {e}")
+
+# # Beispiel: Informationen über das Cluster abrufen
+# schema = client.schema.get()
+# print("Schema im Cluster:", schema)
